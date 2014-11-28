1a7ca732fb5b5c19e97d13138ace057d4804df62
SeongSoo.Cho <nexusz99@a2company.co.kr>
2014-11-28 18:17:34 +0900

    debian Packaging 정보 추가

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup
 
-setup(
-    setup_requires=['pbr'],
-    pbr=True
-)
+import swiftelklogging
+
+setup(name='swift_elk_logging',
+      version=swiftelklogging.version,
+      description='Middleware for logging to elk system',
+      license='Apache License (2.0)',
+      author='a2company',
+      author_email='admin@a2company.co.kr',
+      packages=['swiftelklogging'],
+      install_requires=['swift >= 1.13.0'],
+      entry_points={'paste.filter_factory':
+                        ['swift_elk_logging='
+                         'swiftelklogging.middleware:filter_factory']})
